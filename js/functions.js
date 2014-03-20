ed2574eee2e16bbc23189931633cf55160007714
guilhermeZida <guiryl@gmail.com>
2014-03-20 15:50:33 -0300

    Html frame
    Html frame whit .scss

---
+++ js/functions.js
@@ -0,0 +1,12 @@
+$(document).ready(function(){
+
+	$(window).load(function(){
+		$(".crop").fullscreenBackground();
+		$(".fit-video").fitVids();
+	});
+
+	$('#slider1').sliderTouch({
+		nav:"dots"
+	});
+
+});
